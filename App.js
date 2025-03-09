dadf88a70b6b7c053318a30a9fb8c566ea875e1a
Salwiyadav1408 <144599760+Salwiyadav1408@users.noreply.github.com>
2025-03-09 12:00:28 +0530

    Added index.html, index.css, App.js, index.js

---
+++ App.js
@@ -0,0 +1,15 @@
+import ReactDOM from "react-dom/client";
+import SortingVisualizer from "../components";
+
+function App() {
+
+  return(
+    <SortingVisualizer></SortingVisualizer>
+
+  )
+  
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
+
+
